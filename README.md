00a5662536215759916388c497562306df74214b
kiethohoai <kietho.060193@gmail.com>
2024-08-02 16:24:19 +0700

    004 Setting Up a Professional File Structure

---
+++ README.md
@@ -0,0 +1,1 @@
+004 Setting Up a Professional File Structure
---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,20 @@
+/* eslint react/prop-types: 0 */
+import React from "react";
+import { formatCurrency } from "../../utils/helpers";
+
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
